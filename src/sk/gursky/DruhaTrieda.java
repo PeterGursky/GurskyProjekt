802eb3dc1d6953fc9b36906e58420520b3956da0
student <student@158.197.35.29>
2022-09-28 18:23:23 +0200

    zla trieda

--- src/sk/gursky/DruhaTrieda.java
+++ src/sk/gursky/DruhaTrieda.java
@@ -3,7 +3,7 @@ package sk.gursky;
 public class DruhaTrieda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Intruder: toto je zla trieda");
 
 	}
 

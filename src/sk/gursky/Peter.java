f412f46856f84505ac1eb85b4543241d1bcf83dc
student <student@158.197.35.29>
2022-10-05 16:24:49 +0200

    funkcionalita 1 - vypis

--- src/sk/gursky/Peter.java
+++ src/sk/gursky/Peter.java
@@ -5,6 +5,7 @@ public class Peter {
 	public static void main(String[] args) {
 		System.out.println("Ahoj Github");
 		System.out.println("PAZ1c je super");
+		System.out.println("Funkcionalita 1");
 	}
 
 }
